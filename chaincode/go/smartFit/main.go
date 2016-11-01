52b1a5a5832cc807304500719e222c7b994e0d2a
Mike Aro <arom@us.ibm.com>
2016-11-01 11:12:00 -0500

    update smart fit

--- chaincode/go/smartFit/main.go
+++ chaincode/go/smartFit/main.go
@@ -15,10 +15,39 @@ package main
 import (
 	"encoding/base64"
 	"encoding/binary"
+	"encoding/json"
 	"errors"
+	"fmt"
 	"strconv"
 	"strings"
-
+    "time"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
-	"github.com/op/go-logging"
 )
+
+
+// SimpleChaincode example simple Chaincode implementation
+type SimpleChaincode struct {
+
+}
+
+const CONTRACTSTATEKEY string = "ContractStateKey"  
+// store contract state - only version in this example
+const MYVERSION string = "1.0"
+
+// ************************************
+// contract state 
+// ************************************
+
+
+type ContractState struct {
+    Version      string                        `json:"version"`
+}
+
+
+type SmartFitState struct {
+    AssetID          string      `json:"assetid,omitempty"`        // all assets must have an ID, primary key of contract
+    ActiveMinutes    string      `json:"activeminutes,omitempty"`       
+    CaloriesOut      string      `json:"caloriesout,omitempty"`    
+    Steps            string      `json:"steps,omitempty"`
+    Timestamp        string      `json:"timestamp,omitempty"`        
+}
