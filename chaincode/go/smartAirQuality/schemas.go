d694288e4ea07e54083c95e2ee7ffc0699330baa
Mike Aro <arom@us.ibm.com>
2016-11-05 00:35:13 -0500

    updated schema

--- chaincode/go/smartAirQuality/schemas.go
+++ chaincode/go/smartAirQuality/schemas.go
@@ -54,6 +54,134 @@ var schemas = `
                },
 
         }, //createAsset
+        "deleteAsset": {
+            "description": "Delete an asset. Argument is a JSON encoded string containing only an assetID.",
+            "properties": {
+                "args": {
+                    "description": "args are JSON encoded strings",
+                    "items": {
+                        "description": "An object containing only an assetID for use as an argument to read or delete.",
+                        "properties": {
+                            "assetID": {
+                                "description": "The ID of a managed asset. The resource focal point for a smart contract.",
+                                "type": "string"
+                            }
+                        },
+                        "type": "object"
+                    },
+                    "maxItems": 1,
+                    "minItems": 1,
+                    "type": "array"
+                },
+                "function": {
+                    "description": "deleteAsset function",
+                    "enum": [
+                        "deleteAsset"
+                    ],
+                    "type": "string"
+                },
+                "method": "invoke"
+            },
+            "type": "object"
+        },
+        "init": {
+            "description": "Initializes the contract when started, either by deployment or by peer restart.",
+            "properties": {
+                "args": {
+                    "description": "args are JSON encoded strings",
+                    "items": {
+                        "description": "event sent to init on deployment",
+                        "properties": {
+                            "nickname": {
+                                "default": "SIMPLE",
+                                "description": "The nickname of the current contract",
+                                "type": "string"
+                            },
+                            "version": {
+                                "description": "The ID of a managed asset. The resource focal point for a smart contract.",
+                                "type": "string"
+                            }
+                        },
+                        "required": [
+                            "version"
+                        ],
+                        "type": "object"
+                    },
+                    "maxItems": 1,
+                    "minItems": 1,
+                    "type": "array"
+                },
+                "function": {
+                    "description": "init function",
+                    "enum": [
+                        "init"
+                    ],
+                    "type": "string"
+                },
+                "method": "deploy"
+            },
+            "type": "object"
+        },
+        "readAsset": {
+            "description": "Returns the state an asset. Argument is a JSON encoded string. AssetID is the only accepted property.",
+            "properties": {
+                "args": {
+                    "description": "args are JSON encoded strings",
+                    "items": {
+                        "description": "An object containing only an assetID for use as an argument to read or delete.",
+                        "properties": {
+                            "assetID": {
+                                "description": "The ID of a managed asset. The resource focal point for a smart contract.",
+                                "type": "string"
+                            }
+                        },
+                        "type": "object"
+                    },
+                    "maxItems": 1,
+                    "minItems": 1,
+                    "type": "array"
+                },
+                "function": {
+                    "description": "readAsset function",
+                    "enum": [
+                        "readAsset"
+                    ],
+                    "type": "string"
+                },
+                "method": "query",
+                "result": {
+                    "description": "A set of fields that constitute the complete asset state.",
+                    "properties": {
+                        "assetID": {
+                            "description": "The ID of a managed asset. The resource focal point for a smart contract.",
+                            "type": "string"
+                        },
+                        "carrier": {
+                            "description": "transport entity currently in possession of asset",
+                            "type": "string"
+                        },
+                        "location": {
+                            "description": "A geographical coordinate",
+                            "properties": {
+                                "latitude": {
+                                    "type": "number"
+                                },
+                                "longitude": {
+                                    "type": "number"
+                                }
+                            },
+                            "type": "object"
+                        },
+                        "temperature": {
+                            "description": "Temperature of the asset in CELSIUS.",
+                            "type": "number"
+                        }
+                    },
+                    "type": "object"
+                }
+            },
+            "type": "object"
+        },
        "readAssetSamples": {
             "description": "Returns a string generated from the schema containing sample Objects as specified in generate.json in the scripts folder.",
             "properties": {
