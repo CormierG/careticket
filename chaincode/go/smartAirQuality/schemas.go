a850e720d4026c490927b1080e9305b0c8ed8282
Mike Aro <arom@us.ibm.com>
2016-11-05 00:44:20 -0500

    updated schema

--- chaincode/go/smartAirQuality/schemas.go
+++ chaincode/go/smartAirQuality/schemas.go
@@ -282,7 +282,100 @@ var schemas = `
                    "method": "invoke"
                },
                "type": "object"
-        } //updateAsset
+        }, //updateAsset
+        "objectModelSchemas": {
+        "assetIDKey": {
+            "description": "An object containing only an assetID for use as an argument to read or delete.",
+            "properties": {
+                "assetID": {
+                    "description": "The ID of a managed asset. The resource focal point for a smart contract.",
+                    "type": "string"
+                }
+            },
+            "type": "object"
+        },
+        "event": {
+            "description": "A set of fields that constitute the writable fields in an asset's state. AssetID is mandatory along with at least one writable field. In this contract pattern, a partial state is used as an event.",
+              "properties": {
+                   "args": {
+                       "description": "args are JSON encoded strings",
+                       "items": {
+                           "description": "A set of fields that constitute the writable fields in an asset's state. AssetID is mandatory along with at least one writable field. In this contract pattern, a partial state is used as an event.",
+                           "properties": {
+                               "assetID": {
+                               		"description": "The ID of a managed asset. The resource focal point for a smart contract.",
+                               		"type": "string"
+                               },
+                               "roomtype": {
+
+                               },
+                               "locationname": {
+                               		"type": "string"
+                               },
+                               "latitude": {
+                               		"description": "A geographical coordinate",
+                               		"type": "number"
+                               },
+                               "longitude": {
+                               		"description": "A geographical coordinate",
+                               		"type": "number"
+                               },
+                               "ownertype": {
+                               		"type": "string"
+                               }
+				 								
+                           },
+            "required": [
+                "assetID"
+            ],
+            "type": "object"
+        },
+         "initEvent": {
+            "description": "event sent to init on deployment",
+            "properties": {
+                "nickname": {
+                    "default": "SIMPLE",
+                    "description": "The nickname of the current contract",
+                    "type": "string"
+                },
+                "version": {
+                    "description": "The ID of a managed asset. The resource focal point for a smart contract.",
+                    "type": "string"
+                }
+            },
+            "required": [
+                "version"
+            ],
+            "type": "object"
+        },
+        "state": {
+            "description": "A set of fields that constitute the complete asset state.",
+                           "properties": {
+                               "assetID": {
+                               		"description": "The ID of a managed asset. The resource focal point for a smart contract.",
+                               		"type": "string"
+                               },
+                               "roomtype": {
+
+                               },
+                               "locationname": {
+                               		"type": "string"
+                               },
+                               "latitude": {
+                               		"description": "A geographical coordinate",
+                               		"type": "number"
+                               },
+                               "longitude": {
+                               		"description": "A geographical coordinate",
+                               		"type": "number"
+                               },
+                               "ownertype": {
+                               		"type": "string"
+                               }
+            },
+            "type": "object"
+        }
+        
 
 	} //API
 
