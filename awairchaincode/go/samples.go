447053a17a1ae2c78f6186bb059ca7cdaf4bc62c
Mike Aro <arom@us.ibm.com>
2016-11-15 00:14:21 -0600

    Committed chaincode for Awair device

---
+++ awairchaincode/go/samples.go
@@ -0,0 +1,25 @@
+package func main
+
+var samples = `
+{
+	"event": {
+		"assetID": "The ID of an AWAIR device. The resource focal point for a smart contract.",
+		"co2": 504.6,
+		"dust": 13.958333333333334,
+		"temp": 14.628,
+		"humid": 34.514,
+		"voc": 489.8
+	},
+	"initEvent": {
+        "nickname": "AWAIR",
+        "version": "The ID of an AWAIR device. The resource focal point for a smart contract."
+    },
+    "state": {
+		"assetID": "The ID of an AWAIR device. The resource focal point for a smart contract.",
+		"co2": 504.6,
+		"dust": 13.958333333333334,
+		"temp": 14.628,
+		"humid": 34.514,
+		"voc": 489.8
+    }
+}`
